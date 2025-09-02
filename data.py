82d4956d102f8cb77a60b7d847a85627b6a88798
nicolas mesrine <nicolas.mesrine@epitech.eu>
2025-09-02 13:14:35 +0000

    feat: add class for data handling

---
+++ data.py
@@ -0,0 +1,17 @@
+class BaseDataProgram:
+    def read(self):
+        raise NotImplementedError
+
+    def write(self, balance: float):
+        raise NotImplementedError
+
+class DataProgram(BaseDataProgram):
+    def __init__(self):
+        self._storage_balance = 1000.00  # Initial balance
+
+    def read(self):
+        return self._storage_balance
+
+    def write(self, balance: float):
+        self._storage_balance = balance
+        return self._storage_balance
---
+++ tests/test_data.py
@@ -0,0 +1,10 @@
+from data import DataProgram
+
+def test_starting_money():
+    data = DataProgram()
+    assert data.read() == 1000, "Starting balance should be 1000.0"
+
+def test_read_write_money():
+    data = DataProgram()
+    data.write(500)
+    assert data.read() == 500, "Balance should be 500.0"
